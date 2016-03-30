498df9c89495ee2686301bea941cf48c2d70adfc
Louis <loulegrain@gmail.com>
2016-03-31 00:18:25 +0200

    Script qui fonctionne ! Mais grep liste pas tous les fichiers d'un coup

--- Musicleaner.sh
+++ Musicleaner.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo -e "\n########## Script de purge de dossiers de musique ##########"
-echo "Vider les dossiers de musique des fichiers superflus (thumbs et autres..)"
-
-#Allez c'est parti
+# V1.0
+echo -e "\n########## Nettoyage de dossiers de musique ##########"
+echo -e "\nPurger les fichiers superflus (thumbs, nfo, txt et autres..)"
 
 #On définit par défaut le chemin de recherche ~/Musique
 #Et un truc bidon pour la saisie, sinon il rentre jamais dans le while..
 pathScan="$HOME/Musique"
 saisiePathScan="carotte"
+launchPath="`( cd \"$lauchpath\" && pwd )`"
 
 #Tant que la saisie sera pas un dossier existant
 while [ ! -e $saisiePathScan ]
 do
-	echo -e "\n > Entrez le chemin du dossier ou commencera le scan \n(laisser vide pour ~/Musique)"
+	echo -en "\n> Dossier a scanner (laisser vide pour ~/Musique)"
 	read saisiePathScan
 #Si y'a une entrée, on éxécute, sinon on garde ~/Musique
 	if [ ! -z $saisiePathScan ]
@@ -26,7 +26,7 @@ do
 		echo "Le dossier $saisiePathScan semble ne pas exister, essaie encore!"
 		fi
 	else
-	echo "Flemmard ! Bon va pour $pathScan"
+	echo "Le scan s'effectuera dans $pathScan"
 	fi
 done
 
@@ -34,8 +34,23 @@ done
 cd "$pathScan"
 tailleAvant=`du -sh`
 echo "Taille :$tailleAvant"
-echo -e "\nDébut du scan..."
-
-#La ça se complique sévèrement... Je dois mettre dans une liste :
-#Tout ce qui n'est ni du son (.mp3/.m4a/.wav/.flac/.ogg...) ni des images(.jpg/.jpeg/.png)
-mp3s=`ls -R | grep .mp3 > list`
+echo -en " Début du scan..."
+touch deleteList.txt
+# listage des fichiers (-v = sauf)
+find -type f | grep -v -e .mp3 -e .flac -e .wav -e .wma -e .m4a -e .ogg -e .zip -e .rar -e .jpg -e .jpeg -e .JPG -e .png -e .PNG > deleteList.txt
+echo -e "Scan terminé ! \nVérifiez la liste des fichiers a supprimer, puis enregistrez la et quittez l'éditeur."
+sleep 4
+gedit deleteList.txt
+echo -en "\nVoulez vous vraiment supprimer ces fichiers ? (O/N)"
+read conf
+    if [[ "$conf" == "o" ]]
+    then
+     while read i ; do read i ; rm -v "$i" ; done < deleteList.txt
+     echo -e "Suppression effectuée !"
+    else
+     echo -e "Suppression annulée"
+    fi
+ rm deleteList.txt
+tailleApres=`du -sh`
+echo "Taille :$tailleApres"
+exit 0;
